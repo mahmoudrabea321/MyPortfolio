8169682eb5f881cb3c5a94985d84b3aba9ad6caa
Mahmoud Rabea Assaf <bodamomo2010@gmail.com>
2025-09-29 10:56:20 +0300

    toijv ke  n

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,11 +9,15 @@ export default function AboutMe() {
         <div className="order-2 md:order-1">
           <h2 className="text-4xl font-bold mb-6 text-white">About Me</h2>
           <p className="text-lg text-amber-50 mb-6">
-            Hi, I’m <span className="font-semibold">Mahmoud Rabea</span>, a passionate 
-            <span className="text-indigo-500"> Full-Stack Web Developer</span> with a 
-            background in biotechnology and years of experience in customer service, 
-            teaching, and coding. I enjoy building clean, scalable applications that solve 
-            real-world problems.
+           
+             I’m <span className="font-semibold">Mahmoud Rabea</span>, a passionate <span className="text-indigo-500"> Full-Stack Web Developer</span> with a background in biotechnology and a strong drive for building modern, scalable, and user-friendly web applications.
+
+             Over the past few years, I’ve combined my technical expertise with my experience in teaching, customer service, and problem-solving to deliver digital solutions that not only work but make an impact. I specialize in technologies like **React, Node.js, Express, MongoDB, and Tailwind CSS**, and I enjoy turning ideas into functional applications that help people in their everyday lives.
+
+             I’m always eager to learn new tools, collaborate with teams, and explore creative approaches to web development. My goal is to create clean, efficient, and innovative solutions while constantly improving my skills and staying up to date with the latest industry trends.
+
+             When I’m not coding, you’ll probably find me exploring new technologies, solving problems, or brainstorming ways to make digital experiences more seamless.
+
           </p>
           <p className="text-lg text-amber-50">
             My focus is on modern web technologies like 
